bebbd3d1aba0fb4a761b004d2d1ea49eafe50321
Jakob Wassertheurer <jwassertheurer.mmt-b2024@fh-salzburg.ac.at>
2026-03-08 01:48:50 +0100

    add WASM support

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"embed"
 	"log"
+	"runtime"
 	"totally-goated/game"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+//go:embed assets/*
+var assets embed.FS
+
 func main() {
+	game.InitAssets(assets)
 	ebiten.SetWindowSize(game.ScreenWidth, game.ScreenHeight)
 	ebiten.SetWindowTitle("Totally Goated")
-	ebiten.SetFullscreen(true)
+	if runtime.GOOS != "js" {
+		ebiten.SetFullscreen(true)
+	}
 	if err := ebiten.RunGame(game.NewGame()); err != nil {
 		log.Fatal(err)
 	}
